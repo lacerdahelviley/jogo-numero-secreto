ad4049cf826b7dcf0933f4025b86242b31dad46f
Helviley Lacerda <100769784+lacerdahelviley@users.noreply.github.com>
2023-08-10 20:21:26 -0300

    refactor: Correção de funções

--- app/sotearNumero.js
+++ app/sotearNumero.js
@@ -6,7 +6,6 @@ function gerarNumeroAleatorio() {
   return parseInt(Math.random() * maiorValor + 1);
 }
 
-console.log("O número secreto é:", numeroSecreto);
 
 const elementoMenorValor = document.getElementById("menor-valor");
 elementoMenorValor.innerHTML = menorValor;
--- app/validacao.js
+++ app/validacao.js
@@ -2,33 +2,42 @@ function verificaSeOChutePossuiValorValido(chute) {
   const numero = +chute;
 
   const numeros = {
-    'zero zero': 0,
-    '00': 0,
-    '01': 1,
-    'um': 1,
-    'dois': 2,
-    'três': 3,
-    'quatro': 4,
-    'cinco': 5,
-    'seis': 6,
-    'sete': 7,
-    'oito': 8,
-    'nove': 9,
-    'dez': 10
- }
- 
- const corrigeNumeros = (palavra) => {
-    for(numero in numeros){
-       if(palavra === numero){
-          palavra = numeros[numero];   
-       }         
+    "zero zero": 0,
+    "00": 0,
+    "01": 1,
+    um: 1,
+    dois: 2,
+    três: 3,
+    quatro: 4,
+    cinco: 5,
+    seis: 6,
+    sete: 7,
+    oito: 8,
+    nove: 9,
+    dez: 10,
+  };
+
+  const corrigeNumeros = (palavra) => {
+    for (numero in numeros) {
+      if (palavra === numero) {
+        palavra = numeros[numero];
+      }
     }
     return palavra;
- }
+  };
 
   if (chuteForInvalido(numero)) {
-    elementoChute.innerHTML += "<div>Valor inválido</div>";
-    return;
+    if (chute.toUpperCase() === "GAME OVER") {
+      document.body.innerHTML = `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-game-over" >Jogar novamente</button>
+                `;
+      document.body.style.backgroundColor = "black";
+    } else {
+      elementoChute.innerHTML += "<div>Valor inválido</div>";
+      return;
+    }
   }
   if (numeroForMaiorOuMenorQueOvalorPermitido(numero)) {
     elementoChute.innerHTML += `Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}`;
